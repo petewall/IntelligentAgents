a84f3c732ee32351eee0ab045665ee1f7ea8ddbf
Pete Wall <pete@petewall.net>
2015-12-02 10:28:39 -0600

    Adding more stats

--- src/edu/umn/kylepete/stats/VehicleStats.java
+++ src/edu/umn/kylepete/stats/VehicleStats.java
@@ -1,5 +1,10 @@
 package edu.umn.kylepete.stats;
 
 public class VehicleStats {
-    private int milesDriven;
+    private static int milesDriven;
+
+    private static long parkedTime = 0;
+    private static long maxParked = 0;
+    private static long minParked = Long.MAX_VALUE; 
+    private static int parkedCount = 0;
 }
